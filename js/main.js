069e9522cec8dc75871954848b6a63696a84c3e5
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-03-18 11:04:51 -0600

    created property for magic 8 ball project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var magic8ball = {};
+
+// SOLUTION
+// OBJECT magic 8-ball
+// PROPERTY list of answers = [it is certain, as I see it yes, most likley, outlook good, outlook hazy, ask again later, better not tell you now, dont count on it, my reply is no, my sources say no, very doubtful]
+
+// FUNCTION ask the magic 8-ball a question about a future event
+// INPUT question
+// shake the 8-ball
+// 8-ball picks random answer from list of answers
+// OUTPUT answer
+// STOP
+
+// RUN FUNCTION ask question WITH INPUT "Will I ever learn JavaScript?"
