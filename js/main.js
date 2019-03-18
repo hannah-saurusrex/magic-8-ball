39990544ac5a2d9ab74f6cd2b22c958f00f12eba
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-03-18 15:52:59 -0600

    wrote the function to generate a random answer when asking a question

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-var magic8ball = {};
+var magic8Ball = {};
+magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "In your dreams", "outlook hazy"]
 
+magic8Ball.askQuestion = function(question) {
+    var randomNumber = Math.random();
+    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumberForListOfAnswers);
+    var answer = this.listOfAnswers[randomIndex];
+
+    console.log(question);
+    console.log(answer);
+};
 // SOLUTION
 // OBJECT magic 8-ball
 // PROPERTY list of answers = [it is certain, as I see it yes, most likley, outlook good, outlook hazy, ask again later, better not tell you now, dont count on it, my reply is no, my sources say no, very doubtful]
