b07c130947fb8377f059e0420119acfaa8d3cafb
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-03-21 14:58:00 -0600

    wrote javascript to prompt question and show answers

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-var magic8Ball = {};
-magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "In your dreams", "outlook hazy"]
-
-magic8Ball.askQuestion = function(question) {
-    var randomNumber = Math.random();
-    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
-    var randomIndex = Math.floor(randomNumberForListOfAnswers);
-    var answer = this.listOfAnswers[randomIndex];
-
-    console.log(question);
-    console.log(answer);
-};
+$$(document).ready(function(){
+ 
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+ 
+    magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+ 
+        $("#answer").text( answer );
+ 
+        console.log(question);
+        console.log(answer);
+    };
+ 
+    var onClick = function() {
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+    };
+ 
+    $("#questionButton").click( onClick );
+ 
+});
 // SOLUTION
 // OBJECT magic 8-ball
 // PROPERTY list of answers = [it is certain, as I see it yes, most likley, outlook good, outlook hazy, ask again later, better not tell you now, dont count on it, my reply is no, my sources say no, very doubtful]
@@ -22,3 +36,5 @@ magic8Ball.askQuestion = function(question) {
 // STOP
 
 // RUN FUNCTION ask question WITH INPUT "Will I ever learn JavaScript?"
+
+});
