405ae3837bbeacd390ccc01a0b611f12a787539e
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-03-22 13:59:36 -0600

    completed writing magic 8 ball function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
  
     var magic8Ball = {};
     
-    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams.", "Definitely!", "It's not likely"];
 
     magic8Ball.askQuestion = function(question){
         var randomNumber = Math.random();
         var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
         var randomIndex = Math.floor(randomNumberForListOfAnswers);
         var answer = this.listOfAnswers[randomIndex];
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
  
         $("#answer").text( answer );
  
@@ -19,12 +20,18 @@ $(document).ready(function(){
  
     var onClick = function() {
         $("#answer").hide();
-        $("#answer").fadeIn(4000);
+        $("#8ball").effect("shake");
+        $("#answer").fadeIn(2500);
         var question = prompt("ASK A YES/NO QUESTION!");
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
         magic8Ball.askQuestion(question);
+        
     };
  
     $("#questionButton").click( onClick ); 
+    $("#answer").hide();
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+
 });
 
 // $("#image").attr("src","image2.jpg");
